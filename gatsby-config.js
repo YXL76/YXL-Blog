92f75ab49631ac8a322cff90f534a505e2b0f3ba
YXL <chenxin.lan.76@gmail.com>
2020-10-14 16:31:19 +0800

    ✨feat(use graphgl api):

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "YXL Blog",
   },
-  plugins: ["gatsby-theme-material-ui"],
+  plugins: [
+    "gatsby-theme-material-ui",
+    {
+      resolve: `gatsby-plugin-typegen`,
+      options: {
+        emitSchema: {
+          outputPath: "src/__generated__/gatsby-types.d.ts",
+          "src/__generated__/gatsby-introspection.json": true,
+        },
+      },
+    },
+  ],
 };
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,66 +16,83 @@ import {
   RssFeedOutlined,
   SearchOutlined,
 } from "@material-ui/icons";
+import { graphql, useStaticQuery } from "gatsby";
 import type { FC } from "react";
 import React from "react";
 
-export const Layout: FC = ({ children }) => (
-  <>
-    <AppBar position="static" color="transparent" elevation={0}>
-      <Container maxWidth="lg">
-        <Box display="flex" py={1}>
-          <Box
-            display="flex"
-            flexGrow={1}
-            justifyContent="flex-start"
-            alignItems="center"
-          >
-            <Typography variant="overline">
-              <Box
-                fontWeight="fontWeightBold"
-                fontSize={24}
-                lineHeight="normal"
-                mr={4}
-              >
-                YXL Blog
-              </Box>
-            </Typography>
-            <Button variant="text" size="large" startIcon={<PagesOutlined />}>
-              Blogs
-            </Button>
-            <Button
-              variant="text"
-              size="large"
-              startIcon={<LocalOfferOutlined />}
+export const Layout: FC = ({ children }) => {
+  const data = useStaticQuery<GatsbyTypes.SiteMetadataQuery>(graphql`
+    query SiteMetadata {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <AppBar position="static" color="transparent" elevation={0}>
+        <Container maxWidth="lg">
+          <Box display="flex" py={1}>
+            <Box
+              display="flex"
+              flexGrow={1}
+              justifyContent="flex-start"
+              alignItems="center"
             >
-              Tags
-            </Button>
-            <Button variant="text" size="large" startIcon={<ArchiveOutlined />}>
-              Categories
-            </Button>
-            <Button variant="text" size="large" startIcon={<FaceOutlined />}>
-              About
-            </Button>
+              <Typography variant="overline">
+                <Box
+                  fontWeight="fontWeightBold"
+                  fontSize={24}
+                  lineHeight="normal"
+                  mr={4}
+                >
+                  {data.site?.siteMetadata?.title}
+                </Box>
+              </Typography>
+              <Button variant="text" size="large" startIcon={<PagesOutlined />}>
+                Blogs
+              </Button>
+              <Button
+                variant="text"
+                size="large"
+                startIcon={<LocalOfferOutlined />}
+              >
+                Tags
+              </Button>
+              <Button
+                variant="text"
+                size="large"
+                startIcon={<ArchiveOutlined />}
+              >
+                Categories
+              </Button>
+              <Button variant="text" size="large" startIcon={<FaceOutlined />}>
+                About
+              </Button>
+            </Box>
+            <Box display="flex" justifyContent="flex-end" alignItems="center">
+              <IconButton color="inherit">
+                <SearchOutlined />
+              </IconButton>
+              <IconButton color="inherit">
+                <NightsStayOutlined />
+              </IconButton>
+              <IconButton color="inherit">
+                <LanguageSharp />
+              </IconButton>
+              <IconButton color="inherit">
+                <RssFeedOutlined />
+              </IconButton>
+            </Box>
           </Box>
-          <Box display="flex" justifyContent="flex-end" alignItems="center">
-            <IconButton color="inherit">
-              <SearchOutlined />
-            </IconButton>
-            <IconButton color="inherit">
-              <NightsStayOutlined />
-            </IconButton>
-            <IconButton color="inherit">
-              <LanguageSharp />
-            </IconButton>
-            <IconButton color="inherit">
-              <RssFeedOutlined />
-            </IconButton>
-          </Box>
-        </Box>
+        </Container>
+      </AppBar>
+      <Container maxWidth="lg">
+        <Box>{children}</Box>
       </Container>
-    </AppBar>
-    <Container maxWidth="lg">
-      <Box>{children}</Box>
-    </Container>
-  </>
-);
+    </>
+  );
+};
