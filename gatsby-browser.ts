ed038c7ca54d85faa37c661b9c681a57af068c55
YXL <chenxin.lan.76@gmail.com>
2020-10-30 17:52:21 +0800

    🎈perf: only load needed fonts

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,7 +1,7 @@
-import "fontsource-noto-sans-sc/400.css";
-import "fontsource-noto-sans-sc/500.css";
-import "fontsource-noto-sans-sc/700.css";
-import "fontsource-source-code-pro/400.css";
+import "fontsource-noto-sans-sc/chinese-simplified-400.css";
+import "fontsource-noto-sans-sc/chinese-simplified-500.css";
+import "fontsource-noto-sans-sc/chinese-simplified-700.css";
+import "fontsource-source-code-pro/latin-400.css";
 import "katex/dist/katex.min.css";
 import "prism-themes/themes/prism-material-dark.css";
 import "./src/styles/global.css";
